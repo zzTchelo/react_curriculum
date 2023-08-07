57b1d045d0b3743f320f75ba6334f88d8481e136
Joao Marcelo <jmarcelo06@outlook.com>
2023-08-06 21:18:39 -0300

    Add contato

--- src/contact/index.jsx
+++ src/contact/index.jsx
@@ -5,15 +5,16 @@ const linkedinIcon = require('../assets/icons8-linkedin-144.png')
 const instagramIcon = require('../assets/icons8-instagram-96.png')
 const outlookIcon = require('../assets/icons8-ms-outlook-144.png')
 const githubIcon = require('../assets/icons8-github-96.png')
-
+const youtubeIcon = require('../assets/icons8-reproduzir-youtube-94.png')
 
 const Contacts = () => {
     return (
         <ul className="contacts">
             <li className="contact"><img src={linkedinIcon} alt="Icone_LinkedIn" />João Marcelo Silva Santos</li>
             <li className="contact"><img src={githubIcon} alt="Icone_Github" />zzTchelo</li>
-            <li className="contact"><img src={instagramIcon} alt="Icone_Instagram" />João Marcelo Silva Santos</li>
+            <li className="contact"><img src={instagramIcon} alt="Icone_Instagram" />@silvasantosjoaomarcelo</li>
             <li className="contact"><img src={outlookIcon} alt="Icone_Outlook" />jmarcelo06@outlook.com</li>
+            <li className="contact"><img src={youtubeIcon} alt="Icone_Youtube" />João Marcelo Silva Santos</li>
         </ul>
     )
 }
