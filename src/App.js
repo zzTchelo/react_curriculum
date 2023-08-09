cc44ba91b6c5d958d128045dd4637cd11f249091
joao Marcelo <jmarcelo06@outlook.com>
2023-08-09 20:41:12 -0300

    Add Rotas

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import {BrowserRouter, Route, Routes } from 'react-router-dom'
+
 import './App.css';
 import Formacao from './main/formacao';
 import Navbar from './navbar';
@@ -14,27 +16,32 @@ function App() {
         </aside>
         <div className='main-content'>
           <Navbar />
-            { experiencias.map(experiencia =>
-              <Experiencia 
-                empresa = {experiencia.empresa}
-                ano = {experiencia.ano}
-                cargo = {experiencia.cargo}
-                cidade = {experiencia.cidade}
-                tecnologias = {experiencia.tecnologias}
-              />
-            )}
+              <Routes>
+                <Route 
+                  path='/Formacoes'
+                  element={formacoes.map(formacao => 
+                  <Formacao 
+                    curso = {formacao.curso}
+                    faculdade = {formacao.faculdade}
+                    cidade = {formacao.cidade}
+                    ano = {formacao.ano}
+                  />
+                  )} 
+                />
 
-
-
-
-            {/* {formacoes.map(formacao => 
-              <Formacao 
-                curso = {formacao.curso}
-                faculdade = {formacao.faculdade}
-                cidade = {formacao.cidade}
-                ano = {formacao.ano}
-              />
-            )} */}
+                <Route
+                  path='/Experiencias'
+                  element={ experiencias.map(experiencia =>
+                    <Experiencia 
+                      empresa = {experiencia.empresa}
+                      ano = {experiencia.ano}
+                      cargo = {experiencia.cargo}
+                      cidade = {experiencia.cidade}
+                      tecnologias = {experiencia.tecnologias}
+                    />
+                  )}
+                />
+              </Routes>    
         </div>
       </div>
     );
--- src/navbar/index.jsx
+++ src/navbar/index.jsx
@@ -1,10 +1,19 @@
 import './navbar.css'
+import { Link, NavLink } from 'react-router-dom'
 
 const Navbar = () => {
     return (
         <ul className='navbar'>
-            <li className='item-navbar'>Formações</li>
-            <li className='item-navbar'>Experiências</li>
+            <li className='item-navbar'>
+                <Link to='/Formacoes'>
+                    Formações
+                </Link>
+            </li>
+            <li className='item-navbar'>
+                <Link to='/Experiencias'>
+                    Experiências
+                </Link>
+            </li>
             <li className='item-navbar'>Certificados</li>
             <li className='item-navbar'>Habilidades</li>
             <li className='item-navbar'>Projetos</li>
