196916233deab8cee6556974823a97dc64816776
joao Marcelo <jmarcelo06@outlook.com>
2023-08-16 21:41:25 -0300

    Adicionando Produtos

--- src/dados/projetos.jsx
+++ src/dados/projetos.jsx
@@ -4,9 +4,33 @@ const projetos = [
         imagem : require('../assets/projetos/img_pokedex.png'),
         nome : 'MyPkDex',
         descricao : 'Um pequeno projeto utilizando a API pública PokeApi, onde reorganizei os dados utilizando o framework Angular, fazendo uso de recursos da ferramenta, como diretivas, bidings, também utilizando local storage e customizações disponibilizadas pelo Angular Material.',
+        ano : '2023',
+        tecnologias : [
+            'Angular',
+            'Json Server'
+        ]
+    },
+    {
+        link : 'https://projeto-lorem-ipsum-store.vercel.app/',
+        imagem : require('../assets/projetos/img_loremIpsumStore.png'),
+        nome : 'LoremIpsumStore',
+        descricao : 'Um pequeno projeto feito em Angular, onde tinha como objetivo simular o funcionamento principal de um e-commerce, feito utilizando recursos de formulário do Angular e o próprio Angular Material',
         ano : '2022',
         tecnologias : [
             'Angular',
+            'Angular Material'
+        ]
+    },
+    {
+        link : 'https://projeto-calculadora-blond.vercel.app/',
+        imagem : require('../assets/projetos/img_calculadoraJS.png'),
+        nome : 'CalculadoraJS',
+        descricao : 'Uma calculadora simples utilizando Javascript Vanilla, HTML e CSS',
+        ano : '2023',
+        tecnologias : [
+            'HTML',
+            'CSS',
+            'Javascript'
         ]
     },
 ]
